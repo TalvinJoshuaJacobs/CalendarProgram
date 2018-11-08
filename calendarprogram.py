4f7f3d79d465890ade36f8fab0fa5235d1ffa777
Talvin <43337507+TalvinJoshuaJacobs@users.noreply.github.com>
2018-11-08 15:42:21 +0000

    Update calendarprogram.py

--- calendarprogram.py
+++ calendarprogram.py
@@ -1,7 +1,8 @@
 userChoice = input ("Welcome! \n \nPlease chose an option by entering the number that corresponds to your choice: \n 1 View all calendar entries \n 2 Write new entries into the calendar \n 3 Edit existing entries \n 4 Exit the program \n \nChoice: ")
 
 while userChoice != '1' or '2' or '3' or '4':
-    userChoice = input ("\nChoice: ")
+    print ("\nSorry, I didn't quite get that ")
+    userChoice = input ("Choice: ")
     break
     
 if userChoice == ("1"):
@@ -22,4 +23,4 @@ elif userChoice == ("4"):
     exit()
     
 else:
-    userChoice = input ("\n Sorry, I did not get that")
+    print ("")

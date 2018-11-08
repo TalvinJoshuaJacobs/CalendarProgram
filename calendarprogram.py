f5a53cef351a558fb3c2952aa7880d87e70aafac
Talvin <43337507+TalvinJoshuaJacobs@users.noreply.github.com>
2018-11-08 14:53:00 +0000

    Update calendarprogram.py

--- calendarprogram.py
+++ calendarprogram.py
@@ -8,26 +8,33 @@ print ("4 Exit the program")
 print ("")
 userChoice = input ("Choice: ")
 
-if userChoice == ("1"):
-    print ("")
-    print ("You have chosen to view all calendar entries")
+while userChoice == "1" or "2" or "3" or "4" :
     
-elif userChoice == ("2"):
-    print ("")
-    print ("You have chosen to write new entries into the calendar")
-
-elif userChoice == ("3"):
-    print ("")
-    print ("You have chosen to edit existing calendar entries")
-
-elif userChoice == ("4"):
-    print ("This program will now close")
-    print ("Thank you for using this program")
-    exit()
-
-else:
-    print ("Sorry, I didn't understand that. Please enter in your input again")
-    print ("")
+    if userChoice == ("1"):
+        print ("")
+        print ("You have chosen to view all calendar entries")
+        break
     
+    elif userChoice == ("2"):
+        print ("")
+        print ("You have chosen to write new entries into the calendar")
+        break
+
+    elif userChoice == ("3"):
+        print ("")
+        print ("You have chosen to edit existing calendar entries")
+        break
+
+    elif userChoice == ("4"):
+        print ("This program will now close")
+        print ("Thank you for using this program")
+        exit()
+        break
+
+    else:
+        print ("Sorry, I didn't understand that. Please enter in your input again")
+        print ("")
+        break
+
 
     

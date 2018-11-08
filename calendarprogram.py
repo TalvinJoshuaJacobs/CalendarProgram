d6311d3b253384f585818c3c8f594d88a5575f18
Talvin <43337507+TalvinJoshuaJacobs@users.noreply.github.com>
2018-11-08 15:30:44 +0000

    Update calendarprogram.py

--- calendarprogram.py
+++ calendarprogram.py
@@ -1,40 +1,27 @@
-print ("Welcome!")
-print ("")
-print ("Please chose an option by entering the number that corresponds to your choice:")
-print ("1 View all calendar entries")
-print ("2 Write new entries into the calendar")
-print ("3 Edit existing entries")
-print ("4 Exit the program")
-print ("")
-userChoice = input ("Choice: ")
+userChoice = input ("Welcome! \n \nPlease chose an option by entering the number that corresponds to your choice: \n 1 View all calendar entries \n 2 Write new entries into the calendar \n 3 Edit existing entries \n 4 Exit the program \n \nChoice: ")
 
-while userChoice == "1" or "2" or "3" or "4" :
+while userChoice != ("1" or "2" or "3" or "4"):
+    userChoice = input ("\nChoice: ")
+    break
     
-    if userChoice == ("1"):
-        print ("")
-        print ("You have chosen to view all calendar entries")
-        break
+if userChoice == ("1"):
+    print ("")
+    print ("You have chosen to view all calendar entries")
+
+elif userChoice == ("2"):
+    print ("")
+    print ("You have chosen to write new entries into the calendar")
+
+elif userChoice == ("3"):
+    print ("")
+    print ("You have chosen to edit existing calendar entries")
     
-    elif userChoice == ("2"):
-        print ("")
-        print ("You have chosen to write new entries into the calendar")
-        break
-
-    elif userChoice == ("3"):
-        print ("")
-        print ("You have chosen to edit existing calendar entries")
-        break
-
-    elif userChoice == ("4"):
-        print ("This program will now close")
-        print ("Thank you for using this program")
-        exit()
-        break
-
-    else:
-        print ("Sorry, I didn't understand that. Please enter in your input again")
-        print ("")
-        break
-
-
+elif userChoice == ("4"):
+    print ("This program will now close")
+    print ("Thank you for using this program")
+    exit()
     
+else:
+    userChoice = input ("\n Sorry, I did not get that")
+
+         
